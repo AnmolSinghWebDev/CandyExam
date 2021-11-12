dd52b926db6dd85479ab481d8e30f4709c748993
Anmol <Anmolsinghwebdev@gmail.com>
2021-11-12 15:08:01 -0500

    Candy exam: build added

---
+++ .nuxt/components/index.js
@@ -0,0 +1,35 @@
+export { default as Header } from '../..\\components\\Header.vue'
+export { default as Home } from '../..\\components\\Home.vue'
+export { default as UtilsBlog } from '../..\\components\\utils\\Blog.vue'
+export { default as UtilsContactForm } from '../..\\components\\utils\\ContactForm.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
