e3d87c80761ef9d0d68375213a3e9a422f8a6525
Anmol <Anmolsinghwebdev@gmail.com>
2021-11-11 21:02:28 -0500

    added featured media API call to get images for blog posts

--- components/utils/Blog.vue
+++ components/utils/Blog.vue
@@ -3,20 +3,48 @@
     <a target="_blank" :href="blog.link">
         <h2>{{blog.title.rendered}} by {{blog.author}} <sub> {{blog.date}}</sub></h2>
         <p v-html="blog.content.rendered"></p>
-        
+        <div class="img" v-if="blog.featured_media !== 0" >
+          
+          <img :src="this.featured">
+        </div>
     </a>
   </div>
 </template>
 <script>
-
+  import axios from 'axios';
+  
   export default {
     name : 'blog',
     props: ['blog', 'id'],
-  }
 
+    mounted () {
+      if (this.blog.featured_media > 0){
+        this.getMedia(this.blog.featured_media)
+      }
+    },
+
+    data () {
+      return{
+        featured : [],
+      }
+    },
+
+    methods: {
+      async getMedia (id) {
+        const config = {
+          headers: {
+            'Accept' : 'application/json',
+          }
+        }
+        
+        const res = await axios.get(`https://exam.candy-dev.com/wp-json/wp/v2/media/${id}`, config);
+        
+        this.featured = res.data.source_url;
+      }  
+    },
+  }
 </script>
 <style scoped>
-  
   .blog{
     border: 1px solid red;
     width: 90%;
